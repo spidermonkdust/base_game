7e099962c2682835d82fcb9d91edc1f8b9b27289
Dustin Mitchell <spidermonkdust@yahoo.com>
2021-09-04 17:06:59 -0500

    Initial commit

---
+++ mods/mapgen/init.lua
@@ -0,0 +1,63 @@
+--
+-- Aliases for map generator outputs
+--
+
+-- ESSENTIAL node aliases
+-- Basic nodes
+minetest.register_alias("mapgen_stone", "basenodes:stone")
+minetest.register_alias("mapgen_water_source", "basenodes:water_source")
+minetest.register_alias("mapgen_river_water_source", "basenodes:river_water_source")
+
+-- Additional essential aliases for v6
+minetest.register_alias("mapgen_lava_source", "basenodes:lava_source")
+minetest.register_alias("mapgen_dirt", "basenodes:dirt")
+minetest.register_alias("mapgen_dirt_with_grass", "basenodes:dirt_with_grass")
+minetest.register_alias("mapgen_sand", "basenodes:sand")
+minetest.register_alias("mapgen_tree", "basenodes:tree")
+minetest.register_alias("mapgen_leaves", "basenodes:leaves")
+minetest.register_alias("mapgen_apple", "basenodes:apple")
+
+-- Essential alias for dungeons
+minetest.register_alias("mapgen_cobble", "basenodes:cobble")
+
+--
+-- Register biomes for biome API
+--
+
+minetest.clear_registered_biomes()
+minetest.clear_registered_decorations()
+
+if minetest.settings:get_bool("devtest_register_biomes", true) then
+	
+	minetest.register_biome({
+		name = "mapgen:sandland",
+		node_top = "basenodes:sand",
+		depth_top = 1,
+		node_filler = "basenodes:sand",
+		depth_filler = 1,
+		node_riverbed = "basenodes:leaves",
+		depth_riverbed = 2,
+		node_dungeon = "basenodes:sandstone",
+		y_max = 31000,
+		y_min = 4,
+		heat_point = 50,
+		humidity_point = 50,
+	})
+
+	minetest.register_biome({
+		name = "mapgen:grassland",
+		node_top = "basenodes:dirt_with_grass",
+		depth_top = 1,
+		node_filler = "basenodes:dirt",
+		depth_filler = 1,
+		node_riverbed = "basenodes:sand",
+		depth_riverbed = 2,
+		node_dungeon = "basenodes:cobble",
+		node_dungeon_alt = "basenodes:mossycobble",
+		node_dungeon_stair = "stairs:stair_cobble",
+		y_max = 31000,
+		y_min = 4,
+		heat_point = 50,
+		humidity_point = 50,
+	})
+end
