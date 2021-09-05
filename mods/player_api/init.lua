ab168b103ffe2f7b7d39f88230b2870c98c94cfe
Dustin Mitchell <spidermonkdust@yahoo.com>
2021-09-04 19:54:46 -0500

    Move wieldhand image and define player hand

--- mods/player_api/init.lua
+++ mods/player_api/init.lua
@@ -33,3 +33,20 @@ minetest.register_on_joinplayer(function(player)
 		30
 	)
 end)
+
+-- Define player hand
+minetest.register_item(":", {
+	type = "none",
+	wield_image = "wieldhand.png",
+	wield_scale = {x = 1, y = 2.5, z = 4},
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level = 0,
+		groupcaps = {
+			crumbly = {times = {[2] = 3.00, [3] = 0.70}, uses = 0, maxlevel = 1},
+			snappy = {times = {[3] = 0.40}, uses = 0, maxlevel = 1},
+			oddly_breakable_by_hand = {times = {[1] = 3.50, [2] = 2.00, [3] = 0.70}, uses = 0}
+		},
+		damage_groups = {fleshy = 1},
+	}
+})
